d4cc7dba7fc0a5a66d9358928c8723cb28a4ec16
Fi <benidiktusvio@gmail.com>
2023-08-23 23:39:32 +0100

    🌎 Refactor.

--- b5m-kbd.c
+++ b5m-kbd.c
@@ -13,32 +13,29 @@
 
 #include "ec_utils.h"
 
-#define MSI_EC_FW_VERSION_ADDRESS 0xA0
-#define MSI_EC_FW_VERSION_LENGTH 12
+#define EC_FW_VERSION_ADDRESS 0xA0
+#define EC_FW_VERSION_LENGTH 12
 
-#define MSI_EC_KB_AUDIOMUTE_LED_ADDRESS 0x2C
-#define MSI_EC_KB_AUDIOMUTE_LED_BIT 2
+#define EC_KBD_AUDIOMUTE_LED_ADDRESS 0x2C
+#define EC_KBD_AUDIOMUTE_LED_BIT 2
 
-#define MSI_EC_KB_MICMUTE_LED_ADDRESS 0x2B
-#define MSI_EC_KB_MICMUTE_LED_BIT 2
+#define EC_KBD_MICMUTE_LED_ADDRESS 0x2B
+#define EC_KBD_MICMUTE_LED_BIT 2
 
-#define MSI_EC_KB_BACKLIGHT_ADDRESS_STATE 0xD3
-#define MSI_EC_KB_BACKLIGHT_MAX_STATE 3
+#define EC_KBD_BACKLIGHT_ADDRESS_STATE 0xD3
+#define EC_KBD_BACKLIGHT_MAX_STATE 3
 
-#define MSI_EC_WEBCAM_ADDRESS 0x2E
-#define MSI_EC_WEBCAM_BIT 1
+static int ec_check_bit(u8 addr, u8 bit, bool *output) {
+  int result;
+  u8 stored;
 
-// static int ec_check_bit(u8 addr, u8 bit, bool *output) {
-//   int result;
-//   u8 stored;
+  result = ec_read(addr, &stored);
+  if (result < 0) return result;
 
-//   result = ec_read(addr, &stored);
-//   if (result < 0) return result;
+  *output = check_bit(stored, bit);
 
-//   *output = check_bit(stored, bit);
-
-//   return 0;
-// }
+  return 0;
+}
 
 static int ec_set_bit(u8 addr, u8 bit) {
   int result;
@@ -77,17 +74,13 @@ static int audiomute_led_set(struct led_classdev *led_cdev, enum led_brightness 
   int result;
   u8 value;
 
-  if (brightness == LED_OFF) {
-    result = ec_unset_bit(MSI_EC_KB_AUDIOMUTE_LED_ADDRESS, MSI_EC_KB_AUDIOMUTE_LED_BIT);
-  } else {
-    result = ec_set_bit(MSI_EC_KB_AUDIOMUTE_LED_ADDRESS, MSI_EC_KB_AUDIOMUTE_LED_BIT);
-  }
+  result = brightness == LED_OFF ? ec_unset_bit(EC_KBD_AUDIOMUTE_LED_ADDRESS, EC_KBD_AUDIOMUTE_LED_BIT)
+                                 : ec_set_bit(EC_KBD_AUDIOMUTE_LED_ADDRESS, EC_KBD_AUDIOMUTE_LED_BIT);
 
-  // Dump EC
-  result = ec_read(MSI_EC_KB_AUDIOMUTE_LED_ADDRESS, &value);
+  result = ec_read(EC_KBD_AUDIOMUTE_LED_ADDRESS, &value);
   if (result < 0) return result;
 
-  printk(KERN_INFO "MSI B5M - AudioMute LED Trigger - EC 0x%02X: 0x%02X\n", MSI_EC_KB_AUDIOMUTE_LED_ADDRESS, value);
+  printk(KERN_INFO "MSI B5M - AudioMute LED Trigger - EC 0x%02X: 0x%02X\n", EC_KBD_AUDIOMUTE_LED_ADDRESS, value);
 
   return result < 0 ? result : 0;
 }
@@ -103,17 +96,13 @@ static int micmute_led_set(struct led_classdev *led_cdev, enum led_brightness br
   int result;
   u8 value;
 
-  if (brightness == LED_OFF) {
-    result = ec_unset_bit(MSI_EC_KB_MICMUTE_LED_ADDRESS, MSI_EC_KB_MICMUTE_LED_BIT);
-  } else {
-    result = ec_set_bit(MSI_EC_KB_MICMUTE_LED_ADDRESS, MSI_EC_KB_MICMUTE_LED_BIT);
-  }
+  result = brightness == LED_OFF ? ec_unset_bit(EC_KBD_MICMUTE_LED_ADDRESS, EC_KBD_MICMUTE_LED_BIT)
+                                 : ec_set_bit(EC_KBD_MICMUTE_LED_ADDRESS, EC_KBD_MICMUTE_LED_BIT);
 
-  // Dump EC
-  result = ec_read(MSI_EC_KB_MICMUTE_LED_ADDRESS, &value);
+  result = ec_read(EC_KBD_MICMUTE_LED_ADDRESS, &value);
   if (result < 0) return result;
 
-  printk(KERN_INFO "MSI B5M - MicMute LED Trigger - EC 0x%02X: 0x%02X\n", MSI_EC_KB_MICMUTE_LED_ADDRESS, value);
+  printk(KERN_INFO "MSI B5M - MicMute LED Trigger - EC 0x%02X: 0x%02X\n", EC_KBD_MICMUTE_LED_ADDRESS, value);
 
   return result < 0 ? result : 0;
 }
@@ -125,27 +114,24 @@ static struct led_classdev micmute_led_cdev = {
     .brightness_set_blocking = micmute_led_set,
 };
 
-static int ec_get_firmware_version(u8 buf[MSI_EC_FW_VERSION_LENGTH + 1]) {
+static int ec_get_firmware_version(u8 buf[EC_FW_VERSION_LENGTH + 1]) {
   int result;
 
-  memset(buf, 0, MSI_EC_FW_VERSION_LENGTH + 1);
-  result = ec_read_seq(MSI_EC_FW_VERSION_ADDRESS, buf, MSI_EC_FW_VERSION_LENGTH);
+  memset(buf, 0, EC_FW_VERSION_LENGTH + 1);
+  result = ec_read_seq(EC_FW_VERSION_ADDRESS, buf, EC_FW_VERSION_LENGTH);
   if (result < 0) return result;
 
-  return MSI_EC_FW_VERSION_LENGTH + 1;
+  return EC_FW_VERSION_LENGTH + 1;
 }
 
 static int __init hello_init(void) {
-  // Declare variables
   int result;
-  // bool micmute_led;
-  u8 buf[MSI_EC_FW_VERSION_LENGTH + 1];
+  u8 buf[EC_FW_VERSION_LENGTH + 1];
 
-  // Get the firmware version
   result = ec_get_firmware_version(buf);
   if (result < 0) return result;
 
-  printk(KERN_INFO "MSI B5M - EC Firmware Version: %s\n", buf);  // Dump EC
+  printk(KERN_INFO "MSI B5M - EC Firmware Version: %s\n", buf);
   for (u8 i = 0x0; i <= 0xF; i++) {
     u8 value;
     u8 address_base = i * 0x10;
@@ -157,41 +143,11 @@ static int __init hello_init(void) {
     }
   }
 
-  // // Check keyboard backlight state
-  // u8 backlight_state;
-  // result = ec_read(MSI_EC_KB_BACKLIGHT_ADDRESS_STATE, &backlight_state);
-  // if (result < 0) return result;
-
-  // printk(KERN_INFO "MSI EC Keyboard Backlight State: %d\n", backlight_state);
-  // printk(KERN_INFO "MSI EC Keyboard Backlight State: %d\n", backlight_state & 0x3);
-
-  // // Set keyboard backlight state
-  // result = ec_write(0xD3, 0x82);
-  // if (result < 0) return result;
-
-  // Register the audiomute LED
   result = led_classdev_register(NULL, &audiomute_led_cdev);
   if (result < 0) return result;
 
-  // printk(KERN_INFO "Registered audiomute LED\n");
-
-  // Register the micmute LED
   result = led_classdev_register(NULL, &micmute_led_cdev);
   if (result < 0) return result;
-
-  // printk(KERN_INFO "Registered micmute LED\n");
-
-  // // Get the Audiomute LED status
-  // result = ec_check_bit(MSI_EC_KB_AUDIOMUTE_LED_ADDRESS, MSI_EC_KB_AUDIOMUTE_LED_BIT, &micmute_led);
-  // if (result < 0) return result;
-
-  // printk(KERN_INFO "MSI EC Keyboard Audiomute LED: %s\n", micmute_led ? "on" : "off");
-
-  // // Get the Micmute LED status
-  // result = ec_check_bit(MSI_EC_KB_MICMUTE_LED_ADDRESS, MSI_EC_KB_MICMUTE_LED_BIT, &micmute_led);
-  // if (result < 0) return result;
-
-  // printk(KERN_INFO "MSI EC Keyboard Micmute LED: %s\n", micmute_led ? "on" : "off");
 
   return 0;
 }
